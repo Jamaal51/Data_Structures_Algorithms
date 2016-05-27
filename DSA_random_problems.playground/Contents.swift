b6e9d59fc2bed1dcfa5e77d7041f210027201599
Jamaal Sedayao <jsedayao@gmail.com>
2016-05-27 16:47:38 -0400

    added

---
+++ DSA_random_problems.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//insertion sort
+
+
+//Design a parking lot
+//Design a payroll system
+// Compare two strings for the same characters. how do we retrieve values from hashmap. How to avoid collision. design a pack of cards (spider solitaire) and improvise it by comparing cards based on priority basis.
+
+class ParkingLot{
+    
+    
+    
+}
